4a995e86598d487a8f1166dd21545a96301c5fd6
SparrowXD-beuh <tajneyashraj15@gmail.com>
2024-04-08 10:40:13 +0530

    last commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ const cheerio = require("cheerio");
 const express = require("express");
 const app = express();
 const path = require("path");
+const ejs = require("ejs");
+const { connectToDatabase, find, insert } = require("./database");
+
 require("dotenv/config");
 
 app.set("view engine", "ejs");
@@ -74,48 +77,28 @@ app.get("/anime/:title", async (req, res) => {
   console.time();
   process.env.PROD ? scraper = process.env.SCRAPER : "http://localhost:8888";
   try {
-    const dub = req.query.dub;
-    const episode = req.query.ep;
-    const anime = await axios
-      .get(`https://api.jikan.moe/v4/anime/${req.query.id}`)
-      .then((anime) => {
-        return anime.data.data;
-      });
+    const { dub, ep: episode, id: mal_id } = req.query;
+    const exists = await find(`${mal_id}-${episode}-${dub}`, "pages");
+    if (exists) return res.send(exists.html);
+    const anime = (await axios.get(`https://api.jikan.moe/v4/anime/${mal_id}`)).data.data;
     // console.log(anime);
-    const searchResults = await axios
-      .get(`${scraper}/search?keyword=${anime.title}`)
-      .then((res) => {
-        return res.data.body;
-      });
-    const searchResult = searchResults.find((element) => {
-      return anime.title.toLowerCase() === element.name.toLowerCase();
-    });
-    console.log(searchResults);
-    const videoid = await axios
-      .get(
-        `${scraper}/videoid?path=${
-          searchResult.href
-        }&ep=${episode}&dub=${dub}`
-      )
-      .then((res) => {
-        return res.data.body;
-      });
-    if (anime.airing == false) {
-      res.render("anime.ejs", { source: `${scraper}/stream?videoid=${videoid}&player=default`, maxEpisodes: anime.episodes, anime, episode, dub });
+    const searchResult = ((await axios.get(`${scraper}/search?keyword=${anime.title}`)).data.body).find(element => anime.title.toLowerCase() === element.name.toLowerCase());
+    const videoid = (await axios.get(`${scraper}/videoid?path=${searchResult.href}&ep=${episode}&dub=${dub}`)).data.body;
+    let maxEpisodes;
+    if (!anime.airing) {
+      maxEpisodes = anime.episodes;
     } else {
-      const maxEpisodes = await axios
-        .get(
-          `${scraper}/episodes?path=${
-            searchResult.href
-          }&dub=${dub}`
-        )
-        .then((res) => {
-          return res.data.body;
-        });
-      const page = await ejs.renderFile(path.join(__dirname, 'public/pages', 'anime.ejs'), data);
-      res.send(page);
-      // res.render("anime.ejs", { source: `${scraper}/stream?videoid=${videoid}&player=default`, maxEpisodes, anime, episode, dub });
+      maxEpisodes = await axios.get(`${scraper}/episodes?path=${searchResult.href}&dub=${dub}`).data.body;
     }
+      const page = await ejs.renderFile(path.join(__dirname, 'public/pages', 'anime.ejs'), { source: `${scraper}/stream?videoid=${videoid}&player=default`, maxEpisodes, anime, episode, dub });
+      const doc = {
+        _id: `${mal_id}-${episode}-${dub}`,
+        html: page
+      };
+      // if (doc.html.length != 0) {
+      //   await insert(doc, 'pages');
+      // }
+      res.send(page);
   } catch (error) {
     console.error("Error fetching anime details:", error);
   } finally {
@@ -123,7 +106,8 @@ app.get("/anime/:title", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, async () => {
+  await connectToDatabase();
   console.log("Server is running on http://localhost:3000");
 });
 
